42b5c8e2f812840e14ac3f506a1e4bfff5ac870b
lemoshugo <33234733+lemoshugo@users.noreply.github.com>
2018-01-21 19:44:44 -0200

    Update BusinessList.js

--- src/BusinessList/BusinessList.js
+++ src/BusinessList/BusinessList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
-export class BusinessList extends React.Component {
+class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
